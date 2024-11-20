72c2ff256d5653ea562a5360c18aa2646e1a52a7
prime-infinity <soe.a.prime@gmail.com>
2024-11-20 11:53:35 -0800

    added indicators

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -3,19 +3,65 @@ import 'package:numeracy_app/screens/categories/category.dart';
 import 'package:numeracy_app/screens/questions/question.dart';
 import 'package:numeracy_app/theme.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final _pageController = PageController(initialPage: 1);
+  int _currentPage = 1;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.white,
-      appBar: AppBar(
+      /*appBar: AppBar(
         title: const Text("Home"),
         centerTitle: true,
-      ),
-      body: PageView(
-        children: const [Category(), Question()],
+      ),*/
+      body: Column(
+        children: [
+          Expanded(
+            child: PageView(
+              controller: _pageController,
+              onPageChanged: (index) {
+                setState(() {
+                  _currentPage = index;
+                });
+              },
+              children: const [Category(), Question()],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
+                  children: List.generate(
+                    2,
+                    (index) => Padding(
+                      padding: const EdgeInsets.only(right: 8),
+                      child: Container(
+                        width: 12,
+                        height: 12,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: _currentPage == index
+                              ? AppColors.primaryColor
+                              : Colors.grey.withOpacity(0.5),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
